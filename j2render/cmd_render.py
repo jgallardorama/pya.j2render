e74191813cf674d1911ca65a3c0a2cb8bbfd2c86
jgallardorama <javier.gallardo.rama@gmail.com>
2022-09-21 17:41:05 +0200

    wip

--- j2render/cmd_render.py
+++ j2render/cmd_render.py
@@ -14,7 +14,7 @@ logger = applogging.LogManager().get_app_logger()
 @click.option("--var-file-dir","var_file_dirs", multiple=True)
 @click.option("--var", "var_values",multiple=True)
 @click.option("--template-dir", "template_dir")
-@click.option("--output", "output_dir")
+@click.option("--output", "output_dir", default="output")
 @click.option("--clean", "-c", "clean", default=False, is_flag=True, help="Clean output directory")
 @click.option("--watch", "-w", "watch", default=False, is_flag=True, help="Watch source folder")
 def command(solution_dir, 
@@ -26,6 +26,14 @@ def command(solution_dir,
             clean,
             watch):
     logger.debug("Running Command Render")
+    
+    if not template_dir:
+        template_dir = os.path.join(solution_dir, "templates")
+    
+    if not var_file_dirs:
+        data_dir = os.path.join(solution_dir, "data")
+        var_file_dirs = [data_dir]
+
     solution = template_render.Solution(solution_dir, output_dir, template_dir, var_file_dirs)
     
     if watch:
---
+++ run_lab.bat
@@ -0,0 +1,1 @@
+j2render render --solution-dir lab --watch
